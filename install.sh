791c80553376d4fbecca5c08982cb4df04115e58
Pedro Duque <euphlduque@gmail.com>
2023-04-25 13:54:31 -0300

    Update

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 sudo apt update && sudo apt upgrade -y
 sudo dpkg --add-architecture i386
 
-sudo add-apt-repository ppa:papirus/papirus
-sudo add-apt-repository ppa:slimbook/slimbook
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo add-apt-repository ppa:flexiondotorg/nvtop
-sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo add-apt-repository ppa:papirus/papirus -y
+sudo add-apt-repository ppa:slimbook/slimbook -y 
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo add-apt-repository ppa:flexiondotorg/nvtop -y
 
 sudo apt update && sudo apt full-upgrade
 
@@ -29,14 +28,13 @@ sudo usermod -aG docker $USER
 
 snap remove firefox snap-store
 
-snap install spotify shotcut --classic 
+snap install shotcut --classic 
+snap install telegram-desktop --classic 
 
-sudo apt-get install kubuntu-desktop --no-install-recommends
-
-sudo apt install gnome-tweaks gnome-software gnome-software-common gnome-software-plugin-flatpak gnome-software-plugin-snap ubuntu-restricted-extras -y
-sudo apt install vlc gimp telegram-desktop steam curl gdebi stacer qbittorrent synaptic xdg-desktop-portal-gnome ca-certificates curl gnupg lsb-release -y
+sudo apt install gnome-tweaks gnome-software gnome-software-common gnome-software-plugin-flatpak gnome-software-plugin-snap -y
+sudo apt install vlc gimp steam curl gdebi stacer qbittorrent synaptic xdg-desktop-portal-gnome ca-certificates curl gnupg lsb-release -y
 sudo apt install curl git dirmngr gpg curl gawk linux-headers-$(uname -r) build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools -y
-sudo apt install papirus-icon-theme gir1.2-gtop-2.0 gir1.2-gudev-1.0 slimbookbattery nvidia-driver-525 obs-studio -y
+sudo apt install papirus-icon-theme gir1.2-gtop-2.0 gir1.2-gudev-1.0 slimbookbattery nvidia-driver-530 obs-studio -y
 sudo apt install --install-recommends winehq-stable -y
 sudo apt install nemo gnome-screenshot gdal-bin redis gnome-tweaks gnome-shell-extension-manager gnome-shell-extensions intel-gpu-tools nvtop -y
 sudo apt-get install spotify-client
@@ -45,20 +43,16 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add python
 asdf plugin-add postgres
-asdf python install latest
+asdf install python latest
 asdf python global latest
-asdf nodejs install latest
+asdf install nodejs latest
 asdf nodejs global latest
-asdf postgres install latest
+asdf install postgres latest
 asdf postgres global latest
-asdf python install 3.7.15
-asdf python install 3.9.16
-asdf postgres install 9.4.26
+asdf install python 3.7.15
+asdf install python 3.9.16
+asdf install postgres 9.4.26
 
-sudo apt remove nautlius
-xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
-gsettings set org.gnome.desktop.background show-desktop-icons false
-gsettings set org.nemo.desktop show-desktop-icons true
-xdg-mime query default inode/directory
+sudo apt remove nautilus
 
 sudo apt update && sudo apt upgrade -y
